6211b736c0b59d600f9984d075d689881b4880c3
estiak309w <estiakemon309@gmail.com>
2018-09-24 19:23:24 +0600

    'first'

---
+++ learning_templates/learning_templates/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.conf.urls import url,include
+from basic_app import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index,name='index'),
+    url(r'^basic_app/', include('basic_app.urls'))
+]
